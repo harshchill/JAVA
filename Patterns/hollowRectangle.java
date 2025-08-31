0705b9add3a114d7198d9e1f3c219932f931757a
harshchill <harshmahto02@gmail.com>
2025-08-31 17:51:28 +0530

    pattern 2

---
+++ Patterns/hollowRectangle.java
@@ -0,0 +1,18 @@
+public class hollowRectangle {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+
+        //outer loop (for rows)
+        for(int i =1 ; i <= n ; i ++){
+            //inner loop (for coloms)
+            for(int j = 1 ; j <= m ; j ++ ){
+                //cell on each rows
+                if(i ==1 || i ==n || j == 1 || j == m){
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
